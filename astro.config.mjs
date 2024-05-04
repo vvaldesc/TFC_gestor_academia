bd844ba41d55625cfdf9dd5a52353e9a77660013
vvaldesc <victor.valdescobos@riberadeltajo.es>
2024-05-04 22:31:39 +0200

    Astro SEO Astro Storyblock Astro Icons y mejorando mainpage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,22 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
 import react from "@astrojs/react";
+import icon from "astro-icon";
+import storyblok from "astro-storyblok";
+
+impor
+
+import metaTags from "astro-meta-tags";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), react()]
+  integrations: [tailwind(), svelte(), react(), icon(), metaTags(),
+    storyblok({
+      accessToken: "<your-access-token>",
+      apiOptions: {
+        region: "es",
+      },
+    }),
+  ]
 });
