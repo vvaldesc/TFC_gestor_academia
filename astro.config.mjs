4e39dc70cbe7fe133a1f5f86c1ccb8c89ae54897
vvaldesc <victor.valdescobos@riberadeltajo.es>
2024-05-05 00:20:11 +0200

    integrando storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
 import icon from "astro-icon";
-import storyblok from "astro-storyblok";
-
-impor
+import storyblok from "@storyblok/astro";
 
 import metaTags from "astro-meta-tags";
 
@@ -13,9 +11,20 @@ import metaTags from "astro-meta-tags";
 export default defineConfig({
   integrations: [tailwind(), svelte(), react(), icon(), metaTags(),
     storyblok({
-      accessToken: "<your-access-token>",
-      apiOptions: {
-        region: "es",
+      accessToken: "dq66cq2mTAWPoyV4KnL3Gwtt",
+      region: "eu",
+      bridge: true,
+      apiOptions: {}, // storyblok-js-client options
+      components: {},
+      componentsDir: "src",
+      enableFallbackComponent: false,
+      customFallbackComponent: "",
+      useCustomApi: false,
+      components: {
+        page: 'components/storyblok/Page',
+        feature: 'components/storyblok/Feature',
+        grid: 'components/storyblok/Grid',
+        teaser: 'components/storyblok/Teaser',
       },
     }),
   ]
