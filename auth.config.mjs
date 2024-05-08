14bb9f76c077cdd98b327aaaae48f668fda77b65
Víctor <daw_victorvaldes@riberadeltajo.es>
2024-05-09 01:08:12 +0200

    añadiendo facebook

--- auth.config.mjs
+++ auth.config.mjs
@@ -1,3 +1,4 @@
+import Facebook from '@auth/core/providers/facebook';
 import Google from '@auth/core/providers/google';
 
 import { defineConfig } from 'auth-astro';
@@ -7,6 +8,10 @@ export default defineConfig({
     Google({
       clientId: import.meta.env.GOOGLE_CLIENT_ID,
       clientSecret: import.meta.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Facebook({
+      clientId: import.meta.env.FACEBOOK_CLIENT_ID,
+      clientSecret: import.meta.env.FACEBOOK_CLIENT_SECRET,
     }),
   ],
 });
