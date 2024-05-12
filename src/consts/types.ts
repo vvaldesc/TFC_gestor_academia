e31cf16dbf0d3b57cd6c23bac5ee2ed83a3787c0
Víctor <daw_victorvaldes@riberadeltajo.es>
2024-05-12 20:54:48 +0200

    Auth functions refactor

--- src/consts/types.ts
+++ src/consts/types.ts
@@ -1,30 +1,43 @@
 export interface Session {
-    user:    User;
-    expires: Date;
+  user: User;
+  expires: Date;
 }
 
 export interface User {
-    name:  string;
-    email: string;
-    image: string;
+  name: string;
+  email: string;
+  image: string;
 }
 
 export interface Client {
-    id:            number;
-    name?:         string;
-    surname?:      string;
-    email:         string;
-    phone_number:  string;
-    address?:      string;
-    city?:         string;
-    bornDate?:     Date;
-    created_at?:   Date;
-    updated_at?:   Date;
-    username?:     string;
-    password?:     string;
-    confirmed?:    boolean;
-    image?:        string;
-    active?:       boolean;
+  id: number;
+  name?: string;
+  surname?: string;
+  email: string;
+  phone_number: string;
+  address?: string;
+  city?: string;
+  bornDate?: Date;
+  created_at?: Date;
+  updated_at?: Date;
+  username?: string;
+  password?: string;
+  confirmed?: boolean;
+  image?: string;
+  active?: boolean;
 }
 
-export type ClientSession = { OAuth: Session } & { client: Client } & { profilePhotoSrc: string };
+export type ClientSession = { OAuth: Session } & { client: Client } & {
+  profilePhotoSrc: string;
+};
+
+export enum SessionState {
+  WithoutSession = "WithoutSession",
+  NeedsRegister = "NeedsRegister",
+  Registered = "Registered",
+}
+
+export interface sessionInfoState {
+  sessionInfo: ClientSession;
+  sessionState: SessionState;
+}
