74062a03c0da44524ba282184dab3f1a0780a6ba
Víctor <daw_victorvaldes@riberadeltajo.es>
2024-05-14 19:44:35 +0200

    Working on framer

--- src/sections/Cards_animated/Cards_animated.jsx
+++ src/sections/Cards_animated/Cards_animated.jsx
@@ -1,24 +1,38 @@
-import { motion } from "framer-motion";
-const [selectedId, setSelectedId] = useState(null)
+import { motion, AnimatePresence } from "framer-motion";
+import React, { useState } from 'react';
+
+import Button_basic_framer from "../../components/buttons/Button_basic_framer/Button_basic_framer";
 
 export default function Cards_animated() {
+  const [selectedId, setSelectedId] = useState(null)
+
+  const items = [
+    { id: 1, title: 'First item', subtitle: 'Subtitle 1' },
+    { id: 2, title: 'Second item', subtitle: 'Subtitle 2' },
+    { id: 3, title: 'Third item', subtitle: 'Subtitle 3' }
+  ]
   return (
- 
-    {items.map(item => (
-      <motion.div layoutId={item.id} onClick={() => setSelectedId(item.id)}>
-        <motion.h5>{item.subtitle}</motion.h5>
-        <motion.h2>{item.title}</motion.h2>
-      </motion.div>
-    ))}
-    
-    <AnimatePresence>
-      {selectedId && (
-        <motion.div layoutId={selectedId}>
+    <>
+      {items.map((item) => (
+        <motion.div layoutId={item.id} onClick={() => setSelectedId(item.id)}>
+          <div className="w-36 h-36 bg-black">
           <motion.h5>{item.subtitle}</motion.h5>
           <motion.h2>{item.title}</motion.h2>
-          <motion.button onClick={() => setSelectedId(null)} />
+          </div>
         </motion.div>
-      )}
-    </AnimatePresence>
+      ))}
+      
+      <AnimatePresence>
+        {selectedId && (
+          items.map((item) => (
+            <motion.div layoutId={selectedId}>
+              <motion.h5>{item.subtitle}</motion.h5>
+              <motion.h2>{item.title}</motion.h2>
+              <motion.button onClick={() => setSelectedId(null)} />
+            </motion.div>
+          ))
+        )}
+      </AnimatePresence>
+    </>
   );
 }
