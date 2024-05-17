ebed76d96e9826dbb0c5fe019c8c61f90742f7a6
Víctor <daw_victorvaldes@riberadeltajo.es>
2024-05-17 21:47:51 +0200

    checkpoint

--- src/sections/Stagger_childrens/Stagger_childrens.jsx
+++ src/sections/Stagger_childrens/Stagger_childrens.jsx
@@ -1,60 +1,44 @@
 import { motion } from "framer-motion";
 
-const div_promo = () => {
-    return (
-        <div className="w-36 h-36 bg-black">
-            {/* Content of the div */}
-            <p>s</p>
-        </div>
-    );
+const DivPromo = ({ children }) => {
+  return (
+    <motion.div
+      variants={{
+        hidden: { scale: 0 },
+        show: { scale: 1 },
+      }}
+      className="w-36 h-36 bg-black"
+    >
+      {children}
+    </motion.div>
+  );
 };
 
 export default function StaggerChildren() {
-    return (
-        <motion.section
-            variants={{
-                hidden: { opacity: 0 },
-                show: {
-                    opacity: 1,
-                    transition: {
-                        staggerChildren: 0.5,
-                    },
-                },
-            }}
-            initial="hidden"
-            animate="show"
-            className="flex flex-wrap justify-center gap-4"
-        >
-            
-            <motion.div 
-                variants={{
-                    hidden: { scale: 0 },
-                    show: { scale: 1 },
-                }}
-            >
-                <div_promo className=""/>
-            </motion.div>
-
-            <motion.div 
-                variants={{
-                    hidden: { scale: 0 },
-                    show: { scale: 1 },
-                }}
-                className="w-36 h-36 bg-black"
-                >
-                <div_promo className=""/>
-            </motion.div>
-
-            <motion.div 
-                variants={{
-                    hidden: { scale: 0 },
-                    show: { scale: 1 },
-                }}
-                className="w-36 h-36 bg-black"
-                >
-                <div_promo className=""/>
-            </motion.div>
-
-        </motion.section>
-    );
+  return (
+    <motion.section
+      variants={{
+        hidden: { opacity: 0 },
+        show: {
+          opacity: 1,
+          transition: {
+            staggerChildren: 0.5,
+          },
+        },
+      }}
+      initial="hidden"
+      animate="show"
+      className="flex flex-wrap justify-center gap-4"
+    >
+    <DivPromo>
+      <p style={{ color: 'white' }}>Contenido</p>
+    </DivPromo>
+    <DivPromo>
+      <p style={{ color: 'white' }}>Contenido</p>
+    </DivPromo>
+    <DivPromo>
+      <p style={{ color: 'white' }}>Contenido</p>
+    </DivPromo>
+    </motion.section>
+  );
 }
