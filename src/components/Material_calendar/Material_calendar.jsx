d78cf042f690f685fd1449643a7b185ca410bd0a
Víctor <daw_victorvaldes@riberadeltajo.es>
2024-05-20 17:43:42 +0200

    StaticDateTimePicker

--- src/components/Material_calendar/Material_calendar.jsx
+++ src/components/Material_calendar/Material_calendar.jsx
@@ -1,22 +1,41 @@
-import * as React from 'react';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import * as React from "react";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import { PickersDay } from "@mui/x-date-pickers/PickersDay";
+import dayjs from "dayjs";
 
-//const unavailableDays = ['2022-12-25', '2023-01-01']; // Example of unavailable days
 
-export default function Material_calendar({ unavailableDays }) {
-    //const [highlightedDays, setHighlightedDays] = React.useState([1, 2, 15]);
+export default function Material_calendar({ unavailableDays = [] }) { // Asigna un valor predeterminado
+  const isDayUnavailable = (day) => {
+    // Verifica si unavailableDays es un arreglo antes de llamar a .some()
+    return Array.isArray(unavailableDays) && unavailableDays.some(
+      (unavailableDay) => dayjs(day).format("YYYY-MM-DD") === unavailableDay
+    );
+  };
 
-    return (
+  return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DateCalendar 
-        disablePast= {true}
-        focusedView= {false}
-        views= {['year', 'month', 'day']}
-        firstDayOfWeek= {1}
-        loading = {false}
-        timezone='system'
+      <DateCalendar
+        disablePast={true}
+        focusedView={false}
+        views={["year", "month", "day"]}
+        firstDayOfWeek={1}
+        loading={false}
+        timezone="system"
+        // view={['month','day']}
+        // Personalizando el renderizado de los días
+        slots={{
+          day: PickersDay,
+        }}
+        slotProps={{
+          day: ({ day }) => ({
+            // Aquí puedes personalizar el estilo o agregar tooltips
+            style: isDayUnavailable(day) ? { backgroundColor: '#ffcccb', color: '#000', pointerEvents: 'none', opacity: 0.5 } : {},
+            // Deshabilitar el clic en días no disponibles
+            disabled: isDayUnavailable(day),
+          }),
+        }}
       />
     </LocalizationProvider>
   );
---
+++ src/components/Material_static_date_time_picker/Material_static_date_time_picker.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { StaticDateTimePicker } from '@mui/x-date-pickers/StaticDateTimePicker';
+import { MINUTES_IN_A_PERIOD } from '@/consts/consts';
+
+export default function Material_static_date_time_picker() {
+    const [value, setValue] = useState(new Date());
+    const [key, setKey] = useState(0);
+
+    const handleAccept = (newValue) => {
+        setValue(newValue);
+        console.log(value);
+
+
+        
+        setKey(prevKey => prevKey + 1); // Re-render the component
+    }
+
+    return (
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <StaticDateTimePicker 
+                key={key}
+                orientation="landscape" 
+                label="Static picker"
+                minutesStep={Number(MINUTES_IN_A_PERIOD)}
+                disablePast={true}
+                autoFocus={true}
+                openTo='day'
+                onAccept={() => handleAccept(newValue)}
+                slotProps={{
+                    root: {
+                        'aria-label': 'Static picker',
+                    },
+                    input: {
+                        'aria-label': 'Static picker',
+                    },
+                    inputInput: {
+                        'aria-label': 'Static picker',
+                    },
+                }}
+            />
+        </LocalizationProvider>
+    );
+}
