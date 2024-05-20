07cfa43ccd8d8396047ea2b80af286cc8431a0ef
Víctor <daw_victorvaldes@riberadeltajo.es>
2024-05-20 13:58:44 +0200

    Material editable table

--- src/components/Material_calendar/Material_calendar.jsx
+++ src/components/Material_calendar/Material_calendar.jsx
@@ -3,16 +3,16 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 
-const unavailableDays = ['2022-12-25', '2023-01-01']; // Example of unavailable days
+//const unavailableDays = ['2022-12-25', '2023-01-01']; // Example of unavailable days
 
-export default function BasicDateCalendar({ unavailableDays }) {
-    const [highlightedDays, setHighlightedDays] = React.useState([1, 2, 15]);
+export default function Material_calendar({ unavailableDays }) {
+    //const [highlightedDays, setHighlightedDays] = React.useState([1, 2, 15]);
 
     return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DateCalendar 
         disablePast= {true}
-        focusedView= {true}
+        focusedView= {false}
         views= {['year', 'month', 'day']}
         firstDayOfWeek= {1}
         loading = {false}
