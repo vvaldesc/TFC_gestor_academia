44003d46e03bf415abacaeb3c803f6171b2f803e
Víctor <daw_victorvaldes@riberadeltajo.es>
2024-05-21 19:20:57 +0200

    UI ready to develop

--- src/components/Material_mail_sending_form/Material_mail_sending_form.jsx
+++ src/components/Material_mail_sending_form/Material_mail_sending_form.jsx
@@ -1,14 +1,15 @@
 // Material_mail_sending_form.jsx
 import React, { useState } from 'react';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
 import useSendEmail from '@/services/client/customhooks/useSendMail'; // Asegúrate de importar correctamente el hook
+import { Input } from 'antd';
+const { TextArea } = Input;
 
 export default function Material_mail_sending_form() {
   const [message, setMessage] = useState('');
   const { sendEmail, error } = useSendEmail();
 
   const handleSubmit = async (event) => {
+    console.log('handleSubmit');
     event.preventDefault();
     await sendEmail(message);
     if (error) {
@@ -19,16 +20,8 @@ export default function Material_mail_sending_form() {
   return (
     <form onSubmit={handleSubmit}>
       <div>
-        <TextField
-          id="outlined-multiline-static"
-          label="Multiline"
-          multiline
-          rows={4}
-          defaultValue="Default Value"
-          onChange={(e) => setMessage(e.target.value)}
-        />
+        <TextArea rows={8} cols={90} placeholder="500 carácteres" maxLength={500} />
       </div>
-      <Button type="submit" variant="contained">Enviar</Button>
     </form>
   );
 }
