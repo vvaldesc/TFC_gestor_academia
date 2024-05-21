e1840881dae1ba7eebd07e27af27ffc05f18b4c4
Víctor <daw_victorvaldes@riberadeltajo.es>
2024-05-21 20:04:29 +0200

    stable

--- src/components/Material_static_date_time_picker/Material_static_date_time_picker.jsx
+++ src/components/Material_static_date_time_picker/Material_static_date_time_picker.jsx
@@ -24,7 +24,7 @@ export default function Material_static_date_time_picker() {
                 disablePast={true}
                 autoFocus={true}
                 openTo='day'
-                onAccept={() => handleAccept(newValue)}
+                onAccept={(newValue) => handleAccept(newValue)}
             />
         </LocalizationProvider>
     );
