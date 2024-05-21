02b5da3e01130bcf4b2653182faa45701a8612d0
Víctor <daw_victorvaldes@riberadeltajo.es>
2024-05-21 13:49:42 +0200

    refreshing

---
+++ src/services/client/logic/Refresh.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { onMount } from "svelte";
+  onMount(() => {
+    console.log("hola");
+    setTimeout(() => {
+      console.log("onload");
+      var url = new URL(window.location.href); // get the current url of page into variable
+      var reloaded = url.searchParams.get("reloaded"); // get the 'reloaded' parameter from the url
+      if (!reloaded) {
+        // 'reloaded' parameter does not exist
+        url.searchParams.set("reloaded", "true"); // add the 'reloaded' parameter to the url
+        console.log(url.href);
+        window.location = url.href; // "reload" the page
+      }
+    }, 1000);
+  });
+</script>
