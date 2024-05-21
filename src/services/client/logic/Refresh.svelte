8c99ad3a69be274c09657a8010f038a6ccba0853
Víctor <daw_victorvaldes@riberadeltajo.es>
2024-05-21 18:12:38 +0200

    AntDesign

--- src/services/client/logic/Refresh.svelte
+++ src/services/client/logic/Refresh.svelte
@@ -1,17 +1,16 @@
 <script>
   import { onMount } from "svelte";
   onMount(() => {
-    console.log("hola");
     setTimeout(() => {
       console.log("onload");
       var url = new URL(window.location.href); // get the current url of page into variable
-      var reloaded = url.searchParams.get("reloaded"); // get the 'reloaded' parameter from the url
-      if (!reloaded) {
+      var render = url.searchParams.get("render"); // get the 'reloaded' parameter from the url
+      if (!render) {
         // 'reloaded' parameter does not exist
-        url.searchParams.set("reloaded", "true"); // add the 'reloaded' parameter to the url
+        url.searchParams.set("render", "true"); // add the 'reloaded' parameter to the url
         console.log(url.href);
         window.location = url.href; // "reload" the page
       }
-    }, 60);
+    }, 40);
   });
 </script>
