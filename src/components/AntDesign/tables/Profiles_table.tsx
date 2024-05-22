1eee7fecbbcc6ff7efa544bbc2e881bcbd08e0a3
Víctor <daw_victorvaldes@riberadeltajo.es>
2024-05-22 11:00:45 +0200

    Table displaying data (manager)

--- src/components/AntDesign/tables/Profiles_table.tsx
+++ src/components/AntDesign/tables/Profiles_table.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Space, Table, Tag } from 'antd';
 import type { TableProps } from 'antd';
 
+import useGetProfiles from '@/services/client/customhooks/useGetProfiles';
+
 interface DataType {
   key: string;
   name: string;
@@ -11,78 +13,50 @@ interface DataType {
 }
 
 const columns: TableProps<DataType>['columns'] = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-    render: (text) => <a>{text}</a>,
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-  },
-  {
-    title: 'Tags',
-    key: 'tags',
-    dataIndex: 'tags',
-    render: (_, { tags }) => (
-      <>
-        {tags.map((tag) => {
-          let color = tag.length > 5 ? 'geekblue' : 'green';
-          if (tag === 'loser') {
-            color = 'volcano';
-          }
-          return (
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          );
-        })}
-      </>
-    ),
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: (_, record) => (
-      <Space size="middle">
-        <a>Invite {record.name}</a>
-        <a>Delete</a>
-      </Space>
-    ),
-  },
-];
+  "id",
+  "name",
+  "surname",
+  "email",
+  "phone_number",
+  "address",
+  "city",
+  "bornDate",
+  "created_at",
+  "updated_at",
+  "username",
+  "confirmed",
+  "image",
+  "active",
+  "matriculation_number",
+  "employed",
+  "DNI",
+  "educational_level",
+  "is_admin",
+  "tableName"
+].map((column) => ({
+  title: column,
+  dataIndex: column,
+  key: column,
+}));
 
-const data: DataType[] = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser'],
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sydney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
-  },
-];
+const AntDesign_table: React.FC = () => {
+  const { profiles, loading } = useGetProfiles();
 
-const AntDesign_table: React.FC = () => <Table columns={columns} dataSource={data} />;
+  const data = (profiles?.result?.data.rows || []).map((row: any, index: number) => {
+    const obj: any = {};
+    row.forEach((item: any, i: number) => {
+      obj[columns[i]?.key] = item;
+    });
+    return { key: index, ...obj };
+  });
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Table columns={columns} dataSource={data} />
+  );
+}
 
 export default AntDesign_table;
---
+++ src/services/client/customhooks/useGetProfiles.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const useGetProfiles = () => {
+    const [profiles, setProfiles] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:4321/api/misc/profile/profiles');
+                setProfiles(response.data);
+                setLoading(false);
+            } catch (error) {
+                setError(error);
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return { profiles, loading, error };
+};
+
+export default useGetProfiles;
