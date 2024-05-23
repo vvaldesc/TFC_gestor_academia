bfacee85649ebbfdab0ad683debe6cedc6c514b4
Víctor <daw_victorvaldes@riberadeltajo.es>
2024-05-24 01:19:47 +0200

    LuxonTimer

--- src/components/Material_static_date_time_picker/Material_static_date_time_picker.tsx
+++ src/components/Material_static_date_time_picker/Material_static_date_time_picker.tsx
@@ -4,6 +4,8 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { StaticDateTimePicker } from "@mui/x-date-pickers/StaticDateTimePicker";
 import { MINUTES_IN_A_PERIOD } from "@/consts/consts";
 
+import { AdapterLuxon } from '@mui/x-date-pickers/AdapterLuxon'
+import { DateTime } from 'luxon';
 
 
 import type { ServiceConsumption_type } from "@/models/types";
@@ -11,6 +13,10 @@ import useGetServicePrediction from "@/services/client/customhooks/useGetService
 
 export default function Material_static_date_time_picker({onValueChange}: { onValueChange: (value: any) => void }) {
   const [key, setKey] = useState(0);
+  console.log((DateTime.now()).toString());
+  const dateTimeLuxon = DateTime.local();
+  const minDateTimeLuxon = DateTime.local().minus({days: 1});
+
 
   // const { estimatedTime, loading, error } = useGetServicePrediction(value);
 
@@ -21,12 +27,14 @@ export default function Material_static_date_time_picker({onValueChange}: { onVa
 
   return (
     <>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <LocalizationProvider dateAdapter={AdapterLuxon}>
         <StaticDateTimePicker
           key={key}
           orientation="landscape"
           minutesStep={Number(MINUTES_IN_A_PERIOD)}
-          disablePast={true}
+          // disablePast={true}
+          minDateTime={minDateTimeLuxon}
+          maxDateTime={minDateTimeLuxon.plus({days: 7})}
           autoFocus={true}
           openTo="day"
           onError={(error) => {
@@ -34,7 +42,8 @@ export default function Material_static_date_time_picker({onValueChange}: { onVa
               setKey((prevKey) => prevKey + 1); // Re-render the component
             }}
           onAccept={(newValue) => handleAccept(newValue)}
-          timezone="system"
+          timezone="UTC"
+          views={["day", "hour", "minute"]}
         />
       </LocalizationProvider>
     </>
