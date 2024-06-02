6f92cc1d06a1e866e00a92866212a1a350b92fc1
Víctor <daw_victorvaldes@riberadeltajo.es>
2024-06-02 21:05:36 +0200

    wp set

--- src/sections/Stagger_childrens/Stagger_childrens.jsx
+++ src/sections/Stagger_childrens/Stagger_childrens.jsx
@@ -1,27 +1,28 @@
 import { motion } from "framer-motion";
 import "./Stagger_childrens.css";
 
-const Sphere = () => {
+const Sphere = ({ color }) => {
   return (
     <motion.div
-    style={{ backgroundColor: 'grey' }}
+      style={{ backgroundColor: color }}
       className="sphere"
     />
   );
 };
 
-const Card_promo = ({ children }) => {
+const Card_promo = ({ children, color }) => {
   return (
     <motion.div
       variants={{
         hidden: { scale: 0 },
         show: { scale: 1 },
       }}
-      whileHover={{ scale: 1.5 }}
-      whileFocus={{ scale: 1.1 }}
+      whileHover={{
+         scale: 1.2,
+      }}
       className="section-div-promo"
     >
-      <Sphere/>
+      <Sphere color={color} />
       {children}
     </motion.div>
   );
@@ -45,14 +46,14 @@ export default function StaggerChildren() {
       transition={{ duration: 0.5 }}
       className="flex flex-wrap justify-center gap-4"
     >
-    <Card_promo>
-      <p style={{ color: 'white' }}>Contenido</p>
+    <Card_promo color="red">
+      <p className="main-text-promoTitle" style={{ color: 'white' }}>Matricúlate por menos de 100 euros al mes</p>
     </Card_promo>
-    <Card_promo>
-      <p style={{ color: 'white' }}>Contenido</p>
+    <Card_promo color="blue">
+      <p className="main-text-promoTitle" style={{ color: 'white' }}>Accede a nuestra tienda y compra artículos cosméticos</p>
     </Card_promo>
-    <Card_promo>
-      <p style={{ color: 'white' }}>Contenido</p>
+    <Card_promo color="green">
+      <p className="main-text-promoTitle" style={{ color: 'white' }}>Certificado de experiencia en la estética y peluquería</p>
     </Card_promo>
     </motion.section>
   );
