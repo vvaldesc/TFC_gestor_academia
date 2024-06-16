12fc30ca4d629f2f14a6834d985db7dc62ba605c
Víctor <daw_victorvaldes@riberadeltajo.es>
2024-06-16 12:53:13 +0200

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,12 @@ import icon from "astro-icon";
 import basicSsl from '@vitejs/plugin-basic-ssl';
 import metaTags from "astro-meta-tags";
 import path from 'path'; // Añade esta línea
-import mkcert from 'vite-plugin-mkcert'
+import mkcert from 'vite-plugin-mkcert';
 // import serviceWorker from "astrojs-service-worker";
 import auth from "auth-astro";
 // import netlify from '@astrojs/netlify';
-import vercel from '@astrojs/vercel';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -28,11 +29,13 @@ export default defineConfig({
     server: {
       https: true
     },
-    plugins: [ mkcert({ 
-      savePath: './certs', // save the generated certificate into certs directory
-      autoUpgrade: true, // auto upgrade the certificate if it's expired
-      hosts: ['localhost'], // generate certificate for localhost
-     })],
+    plugins: [mkcert({
+      savePath: './certs',
+      // save the generated certificate into certs directory
+      autoUpgrade: true,
+      // auto upgrade the certificate if it's expired
+      hosts: ['localhost'] // generate certificate for localhost
+    })],
     resolve: {
       alias: {
         '@/*': path.resolve('./src/*'),
@@ -41,18 +44,19 @@ export default defineConfig({
         '@/pages/*': path.resolve('./src/pages/*'),
         '@/services/*': path.resolve('./src/services/*'),
         '@/sections/*': path.resolve('./src/sections/*'),
-        '@/consts/*': path.resolve('./src/consts/*'),
+        '@/consts/*': path.resolve('./src/consts/*')
       }
     }
   },
   integrations: [tailwind(), svelte(), react(), icon({
     include: {
-      mdi: ["*"], // (Default) Loads entire Material Design Icon set
-      bxs: ["*"], // Loads entire bxs set
-    },
+      mdi: ["*"],
+      // (Default) Loads entire Material Design Icon set
+      bxs: ["*"] // Loads entire bxs set
+    }
   }), metaTags(), auth()
   // , serviceWorker()
-],
+  ],
   output: "server",
   adapter: vercel()
 });
